87899c712c9d44f028f7a174710350535749bc9d
“prathmeshgurav” <prathmeshbgurav@gmail.com>
2022-05-23 19:18:58 +0530

    first commit

---
+++ CRUDoperationmvc/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUDoperationmvc.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
